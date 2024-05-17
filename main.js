bdb4b445218930b095904a1ce706fd058359c770
Muzafar Ali <[valid-email]>
2024-05-17 17:02:59 +0500

    nested object push to github

---
+++ main.js
@@ -0,0 +1,13 @@
+const my_Bio = {
+    name: "Muzaffar_Ali",
+    age: 19,
+    Roll: "student",
+    city: "ghotki",
+    coures: {
+        title: "Typescript",
+        duration: "03_months",
+        id_No: 481168
+    }
+};
+console.log(my_Bio);
+export {};
---
+++ main.ts
@@ -0,0 +1,24 @@
+type data = {
+    name:string,
+    age:number,
+    Roll:string,
+    city:string,
+    coures : {
+        title : string,
+        duration:string,
+        id_No :Number
+    }
+}
+const my_Bio:data = {
+    name:"Muzaffar_Ali",
+    age:19,
+    Roll:"student",
+    city:"ghotki",
+    coures : {
+        title : "Typescript",
+        duration:"03_months",
+        id_No :481168
+    }
+}
+console.log(my_Bio);
+
